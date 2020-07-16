9d5e19a144bfe2cea461bb0e3329cec1b51c9145
Bastian Perez <bas.perezp@alumnos.duoc.cl>
2020-07-16 03:38:05 -0400

    Docs agregado

---
+++ docs/precache-manifest.02fd3edea3721c230c06f302d608f508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e594b3bd17efe9bb3e3058a161ca30fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1c2b4425ffac27720ac",
+    "url": "/static/css/main.54644aad.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c2b4425ffac27720ac",
+    "url": "/static/js/main.02037ec9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
